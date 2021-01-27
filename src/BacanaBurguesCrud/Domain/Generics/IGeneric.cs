900566a43c62f0d7223410c174307461d50e4fee
Marcos Vinicius <marcos.viniciuswac@outlook.com>
2021-01-27 20:27:59 -0300

    Aplicando o metodo DDD

---
+++ src/BacanaBurguesCrud/Domain/Generics/IGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Generics
+{
+    interface IGeneric<T> where T: class
+    {
+        Task Add(T Objeto);
+        Task Update(T Objeto);
+        Task task(T Objeto);
+        Task<T> GetEntityById(int id);
+        Task<List<T>> List();
+    }
+}
---
+++ src/BacanaBurguesCrud/Domain/InterfaceProduct/IProduct.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Interfaces.Generics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace Domain.Interfaces.InterfaceProduct
+{
+    interface IProduct:IGeneric<Product>
+    {
+    }
+}
