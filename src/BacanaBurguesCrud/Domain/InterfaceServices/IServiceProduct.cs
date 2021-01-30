fc49185a4beebf178c1755aa150ae09aa47147dd
Marcos Vinicius <marcos.viniciuswac@outlook.com>
2021-01-30 14:49:42 -0300

    Ta andando

---
+++ src/BacanaBurguesCrud/Domain/InterfaceServices/IServiceProduct.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.InterfaceServices
+{
+     public interface IServiceProduct
+    {
+    }
+}
---
+++ src/BacanaBurguesCrud/Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            // Se não estiver configurado no projeto IU pega deginição de chame do json congifurado
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(GetStringConectionConfing());
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        private string GetStringConectionConfing() 
+        {
+            string strCon = "";
+            return strCon;
+
+        
+        }
+
+    }
+}
