88bca4ec93e59be2ffa2dc3b4217ba8d30ac178d
Marcos Vinicius <marcos.viniciuswac@outlook.com>
2021-02-01 15:25:13 -0300

    Fazendo e fazendo

---
+++ src/BacanaBurguesCrud/Infrastructure/Repository/Generics/ReoisutiryGenerics.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repository.Generics
+{
+     public class ReoisutiryGenerics<T> : IGeneric<T>, IDisposable where T: 
+    {
+    }
+}
